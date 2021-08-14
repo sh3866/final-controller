28b9b29464dd19d7ffa50d335034a27c93ced98a
sunghyun <littlepoo2000@gmail.com>
2021-08-14 18:19:07 +0900

    all commit

--- controllers/vvip_controller.go
+++ controllers/vvip_controller.go
@@ -20,18 +20,20 @@ import (
 	"context"
 
 	"k8s.io/apimachinery/pkg/api/errors"
-	"sigs.k8s.io/controller-runtime/pkg/reconcile"
-
 	v1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/apimachinery/pkg/runtime"
-	ctrl "sigs.k8s.io/controller-runtime"
-	"sigs.k8s.io/controller-runtime/pkg/client"
-	"sigs.k8s.io/controller-runtime/pkg/log"
+	"k8s.io/apimachinery/pkg/types"
+	"sigs.k8s.io/controller-runtime/pkg/reconcile"
 
 	"github.com/go-logr/logr"
+	ctrl "sigs.k8s.io/controller-runtime"
+	"sigs.k8s.io/controller-runtime/pkg/client"
+
 	vvipv1 "github.com/vviphw04/vvip-controller/api/v1"
 
 	// APis added
+	"reflect"
+
 	appsv1 "k8s.io/api/apps/v1"
 	corev1 "k8s.io/api/core/v1"
 )
@@ -39,8 +41,8 @@ import (
 // VvipReconciler reconciles a Vvip object
 type VvipReconciler struct {
 	client.Client
-	Log    logr.Logger
-	Scheme *runtime.Scheme
+	Log logr.Logger
+	*runtime.Scheme
 }
 
 //+kubebuilder:rbac:groups=vvip.vviphw.io,resources=vvips,verbs=get;list;watch;create;update;patch;delete
@@ -56,14 +58,29 @@ type VvipReconciler struct {
 //
 // For more details, check Reconcile and its Result here:
 // - https://pkg.go.dev/sigs.k8s.io/controller-runtime@v0.8.3/pkg/reconcile
-func (r *VvipReconciler) Reconcile(ctx context.Context, req ctrl.Request) (ctrl.Result, error) {
-	logger := log.FromContext(ctx)
-	logger.Info("Reconcile logic start.")
+func (r *VvipReconciler) Reconcile(req ctrl.Request) (ctrl.Result, error) {
+	_ = context.Background()
+	reqLogger := r.Log.WithValues("req.Namespace", req.Namespace, "req.Name", req.Name)
+	reqLogger.Info("Reconciling Memcached.")
 	// your logic here
 
-	reqLogger := r.Log.WithValues("req.Namespace", req.Namespace, "req.Name", req.Name)
 	vvip := &vvipv1.Vvip{}
 	err := r.Client.Get(context.TODO(), req.NamespacedName, vvip)
+	if err != nil {
+		if errors.IsNotFound(err) {
+			// Request object not found, could have been deleted after reconcile req.
+			// Owned objects are automatically garbage collected. For additional cleanup logic use finalizers.
+			// Return and don't requeue
+			reqLogger.Info("Vvip resource not found. Ignoring since object must be deleted.")
+			return ctrl.Result{}, nil
+		}
+		// Error reading the object - requeue the req.
+		reqLogger.Error(err, "Failed to get Memcached.")
+		return ctrl.Result{}, err
+	}
+
+	deployment := &appsv1.Deployment{}
+	err = r.Client.Get(context.TODO(), types.NamespacedName{Name: vvip.Name, Namespace: vvip.Namespace}, deployment)
 
 	if err != nil && errors.IsNotFound(err) {
 		dep := r.deploymentForVvip(vvip)
@@ -74,8 +91,24 @@ func (r *VvipReconciler) Reconcile(ctx context.Context, req ctrl.Request) (ctrl.
 			return ctrl.Result{}, err
 		}
 		return reconcile.Result{Requeue: true}, nil
+	} else if err != nil {
+		reqLogger.Error(err, "Failed to get Deployment.")
+		return ctrl.Result{}, err
 	}
 
+	// Ensure the deployment size is the same as the spec
+	size := vvip.Spec.Size
+	if *deployment.Spec.Replicas != size {
+		deployment.Spec.Replicas = &size
+		err = r.Client.Update(context.TODO(), deployment)
+		if err != nil {
+			reqLogger.Error(err, "Failed to update Deployment.", "Deployment.Namespace", deployment.Namespace, "Deployment.Name", deployment.Name)
+			return ctrl.Result{}, err
+		}
+	}
+
+	// Update the Memcached status with the pod names
+	// List the pods for this memcached's deployment
 	podList := &corev1.PodList{}
 	ls := labelsForVvip(vvip.Name)
 	listOps := []client.ListOption{
@@ -90,9 +123,15 @@ func (r *VvipReconciler) Reconcile(ctx context.Context, req ctrl.Request) (ctrl.
 
 	podNames := getPodNames(podList.Items)
 
-	_ = podNames
+	if !reflect.DeepEqual(podNames, vvip.Status.Nodes) {
+		vvip.Status.Nodes = podNames
+		err := r.Client.Status().Update(context.TODO(), vvip)
+		if err != nil {
+			reqLogger.Error(err, "Failed to update Memcached status.")
+			return ctrl.Result{}, err
+		}
+	}
 
-	logger.Info("Nothing happen. Please develop your reconcile logic")
 	return ctrl.Result{}, nil
 }
 
@@ -138,6 +177,29 @@ func (r *VvipReconciler) deploymentForVvip(v *vvipv1.Vvip) *appsv1.Deployment {
 	ctrl.SetControllerReference(v, dep, r.Scheme)
 
 	return dep
+}
+
+// serviceForMemcached function takes in a Memcached object and returns a Service for that object.
+func (r *VvipReconciler) serviceForMemcached(v *vvipv1.Vvip) *corev1.Service {
+	ls := labelsForVvip(v.Name)
+	ser := &corev1.Service{
+		ObjectMeta: v1.ObjectMeta{
+			Name:      v.Name,
+			Namespace: v.Namespace,
+		},
+		Spec: corev1.ServiceSpec{
+			Selector: ls,
+			Ports: []corev1.ServicePort{
+				{
+					Port: 11211,
+					Name: v.Name,
+				},
+			},
+		},
+	}
+	// Set Memcached instance as the owner of the Service.
+	ctrl.SetControllerReference(v, ser, r.Scheme) //todo check how to get the schema
+	return ser
 }
 
 // labelsForVvip returns the labels for selecting the resources
